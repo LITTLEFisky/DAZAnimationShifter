0f5c3b1aca1e166ccba31d94ffa02e43755838ae
LITTLEFisky <fiskblack111@gmail.com>
2023-08-20 19:28:09 +0300

    Added "DONATE!" button
    Because I need money T.T

--- DAZAnimationShifter/Form1.cs
+++ DAZAnimationShifter/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -153,6 +154,11 @@ namespace DAZAnimationShifter
                             "This program shifts all exported frames, so the\n" +
                             "animation starts from frame \"0\", and you can\n" +
                             "place it anywhere on the timeline", "Code (C)LITTLEFisky, 2023", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://boosty.to/littlefisky/single-payment/donation/451573/target?share=target_link");
         }
     }
 }
